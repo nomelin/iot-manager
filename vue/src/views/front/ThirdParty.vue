ec7c536dbe656cb382fbac09f6c4c7838c4b6fd4
nomelin <2023686595@qq.com>
2025-05-31 14:22:50 +0800

    1.3.6 引入第三方网页

---
+++ vue/src/views/front/ThirdParty.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="third-party-page">
+    <iframe
+        src="http://iot.nomelin.top:18999"
+        frameborder="0"
+        class="third-party-iframe"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ThirdParty'
+}
+</script>
+
+<style scoped>
+.third-party-page {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.third-party-iframe {
+  width: 100%;
+  height: calc(100vh - 64px); /* 根据顶部导航栏高度调整 */
+  border: none;
+}
+</style>
